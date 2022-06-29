bbbf3f81e5676f425d397ee5bde198b2442eb5f5
Tiago-Amaral <tiagolima323@gmail.com>
2022-06-29 14:47:09 -0300

    first commit

---
+++ DecolaTec3-edição/src/App.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Pressable,
+  Linking,
+} from 'react-native';
+
+const colorGithub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorDarkFontGithub = '#4F565E';
+
+const imageProfileGithub =
+'https://avatars.githubusercontent.com/u/97295520?v=4';
+const urlToMyGithub = 'https://github.com/Tiago-Amaral';
+
+const App = () => {
+  const handlePressGoToGithub = async () => {
+    console.log('Verificando link');
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      console.log('Link aprovado');
+      console.log('Abrindo link....');
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
+
+  return (
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
+      <View style={style.content}>
+        <Image
+          accessibilityLabel="Tiago com fundo azul degrade"
+          style={style.avatar}
+          source={{uri: imageProfileGithub}}
+        />
+        <Text
+          accessibilityLabel="Nome: Tiago Amaral"
+          style={[style.defaultText, style.name]}>
+         Tiago Amaral
+        </Text>
+        <Text
+          accessibilityLabel="Nickname: Tiago Amaral"
+          style={[style.defaultText, style.nickname]}>
+          Tiago-Amaral
+        </Text>
+        <Text
+          accessibilityLabel="Descrição: Software engineer | Leader tech at Fleye | Mobile Developer | Mentor
+          at Catapulta.club @ismaelmoreiraa | Prof. na DIO"
+          style={[style.defaultText, style.description]}>
+          Eatudante de Programação na Dio.me
+        </Text>
+
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text style={[style.defaultText, style.textButton]}>
+              Open in Github
+            </Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    // Column
+    backgroundColor: colorGithub,
+    flex: 1, // Expandir para a tela inteira
+    justifyContent: 'center',
+    alignItems: 'center',
+    // flexDirection: 'row',
+  },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  avatar: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+  defaultText: {
+    color: colorFontGithub,
+  },
+  name: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  nickname: {
+    fontSize: 18,
+    color: colorDarkFontGithub,
+  },
+  description: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: colorDarkFontGithub,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
